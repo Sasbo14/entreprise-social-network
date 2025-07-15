35aafc7a629c64646cb7176e181ee39b8c8e6c22
Sasbo14 <larbi.ch@hotmail.fr>
2025-07-15 12:10:01 +0200

    Remove files from root after moving backend into folder

--- src/main/java/com/entreprise/socialnetwork/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.entreprise.socialnetwork.repository;
-
-import com.entreprise.socialnetwork.model.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Integer> {
-}
--- src/main/java/com/entreprise/socialnetwork/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.entreprise.socialnetwork.repository;
-
-import com.entreprise.socialnetwork.model.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Integer> {
-}
